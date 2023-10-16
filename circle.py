8f685a1ddfdd12a36568403deb4c805f9168e264
AsmitaSin <148008971+AsmitaSin@users.noreply.github.com>
2023-10-16 23:58:36 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,6 @@
+#circumfrence and area of a circle
+Radius = int(input("Enter radius of the circle:: "))
+area = 3.14*Radius*Radius
+circumfrence = 2*3.24*Radius
+print("Area of Circle",area)
+print("Circumfrence of Circle",circumfrence)
